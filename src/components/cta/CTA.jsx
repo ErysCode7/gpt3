602863a537ddaf956c5ad8b5eef94bfc550c0084
Erys Mozo <mozoerys@gmail.com>
2022-07-23 01:19:49 +0700

    CTA

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./CTA.scss";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h4>Register today & start exploring the endless possiblities.</h4>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </section>
+  );
 };
 
 export default CTA;
